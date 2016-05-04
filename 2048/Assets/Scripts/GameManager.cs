285ce5b6c3acae8a1b7fdf8e2d24dc16094dd6f8
Evgeny <vvevgeny@gmail.com>
2016-05-04 14:02:10 +0300

    end 25 less

--- 2048/Assets/Scripts/GameManager.cs
+++ 2048/Assets/Scripts/GameManager.cs
@@ -1,21 +1,157 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+	private Tile[,] AllTiles = new Tile[4, 4];
+	private List<Tile[]> columns = new List<Tile[]> ();
+	private List<Tile[]> rows = new List<Tile[]> ();
+	private List<Tile> EmptyTiles = new List<Tile>();
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		Tile[] AllTilesOneDim = GameObject.FindObjectsOfType<Tile> ();
+		foreach (Tile t in AllTilesOneDim) 
+		{
+			t.Number = 0;
+			AllTiles [t.indRow, t.indCol] = t;
+			EmptyTiles.Add (t);
+		}
+		columns.Add (new Tile[]{ AllTiles [0, 0], AllTiles [1, 0], AllTiles [2, 0], AllTiles [3, 0] });
+		columns.Add (new Tile[]{ AllTiles [0, 1], AllTiles [1, 1], AllTiles [2, 1], AllTiles [3, 1] });
+		columns.Add (new Tile[]{ AllTiles [0, 2], AllTiles [1, 2], AllTiles [2, 2], AllTiles [3, 2] });
+		columns.Add (new Tile[]{ AllTiles [0, 3], AllTiles [1, 3], AllTiles [2, 3], AllTiles [3, 3] });
+
+		rows.Add (new Tile[]{ AllTiles [0, 0], AllTiles [0, 1], AllTiles [0, 2], AllTiles [0, 3] });
+		rows.Add (new Tile[]{ AllTiles [1, 0], AllTiles [1, 1], AllTiles [1, 2], AllTiles [1, 3] });
+		rows.Add (new Tile[]{ AllTiles [2, 0], AllTiles [2, 1], AllTiles [2, 2], AllTiles [2, 3] });
+		rows.Add (new Tile[]{ AllTiles [3, 0], AllTiles [3, 1], AllTiles [3, 2], AllTiles [3, 3] });
+
+		Generate ();
+		Generate ();
+	}
+
+	bool MakeOneMoveDownIndex(Tile[] LineOfTiles)
+	{
+		for (int i = 0; i < LineOfTiles.Length - 1; i++) 
+		{
+			//MOVE BLOCK
+			if (LineOfTiles [i].Number == 0 && LineOfTiles[i+1].Number !=0) 
+			{
+				LineOfTiles [i].Number = LineOfTiles [i + 1].Number;
+				LineOfTiles [i + 1].Number = 0;
+				return true;
+			}
+			//MERGE BLOCK
+			if (LineOfTiles[i].Number != 0
+				&& LineOfTiles [i].Number == LineOfTiles [i + 1].Number
+				&& LineOfTiles [i].mergedThisTurn == false && LineOfTiles [i + 1].mergedThisTurn == false) 
+			{
+				LineOfTiles [i].Number *= 2;
+				LineOfTiles [i + 1].Number = 0;
+				LineOfTiles [i].mergedThisTurn = true;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool MakeOneMoveUpIndex(Tile[] LineOfTiles)
+	{
+		for (int i = LineOfTiles.Length - 1; i > 0 ; i--) 
+		{
+			//MOVE BLOCK
+			if (LineOfTiles [i].Number == 0 && LineOfTiles[i-1].Number !=0) 
+			{
+				LineOfTiles [i].Number = LineOfTiles [i - 1].Number;
+				LineOfTiles [i - 1].Number = 0;
+				return true;
+			}
+			//MERGE BLOCK
+			if (LineOfTiles[i].Number != 0
+				&& LineOfTiles [i].Number == LineOfTiles [i - 1] .Number
+				&& LineOfTiles [i].mergedThisTurn == false && LineOfTiles [i - 1].mergedThisTurn == false) 
+			{
+				LineOfTiles [i].Number *= 2;
+				LineOfTiles [i - 1].Number = 0;
+				LineOfTiles [i].mergedThisTurn = true;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void Generate()
+	{
+		if (EmptyTiles.Count > 0) 
+		{
+			int indexForNewNumber = Random.Range (0, EmptyTiles.Count);
+
+			int randomNum = Random.Range (0, 5);//20%
+			if(randomNum == 0 )EmptyTiles [indexForNewNumber].Number = 4;
+			else EmptyTiles [indexForNewNumber].Number = 2;
+
+			EmptyTiles.RemoveAt (indexForNewNumber);
+		}
 	}
 	
 	// Update is called once per frame
+	/*
 	void Update () 
     {
+		if (Input.GetKeyDown (KeyCode.G))
+			Generate ();
+	}
+	*/
 
+	private void ResetMergedFlags()
+	{
+		foreach (Tile t in AllTiles)
+			t.mergedThisTurn = false;
+	}
+
+	private void UpdateEmptyTiles()
+	{
+		EmptyTiles.Clear ();
+		foreach (Tile t in AllTiles) 
+		{
+			if (t.Number == 0)
+				EmptyTiles.Add (t);
+		}
 	}
 
     public void Move(MoveDirection md)
     {
         Debug.Log(md.ToString() + " move.");
+		bool moveMade = false;
+
+		ResetMergedFlags ();
+
+		for (int i = 0; i < rows.Count; i++) 
+		{
+			switch (md) 
+			{
+			case MoveDirection.Down:
+				while (MakeOneMoveUpIndex (columns [i])) {moveMade = true;}
+				break;
+			case MoveDirection.Left:
+				while (MakeOneMoveDownIndex (rows [i])) {moveMade = true;}
+				break;
+			case MoveDirection.Right:
+				while (MakeOneMoveUpIndex (rows [i])) {moveMade = true;}
+				break;
+			case MoveDirection.Up:
+				while (MakeOneMoveDownIndex (columns [i])) {moveMade = true;}
+				break;
+			}
+		}
+
+		if (moveMade) 
+		{
+			UpdateEmptyTiles ();
+			Generate ();
+		}
     }
 }
---
+++ 2048/Assets/Scripts/Tile.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour {
+
+	public bool mergedThisTurn = false;
+
+	public int indRow;
+	public int indCol;
+
+	public int Number 
+	{
+		get
+		{
+			return number;
+		}
+		set
+		{
+			number = value;
+			if (number == 0)
+				SetEmpty ();
+			else {
+				ApplyStyle (number);
+				SetVisible ();
+			}
+		}
+	}
+	private int number;
+
+	private Text TileText;
+	private Image TileImage;
+
+	void Awake()
+	{
+		TileText = GetComponentInChildren<Text> ();
+		TileImage = transform.Find ("NumberedCell").GetComponent<Image>();
+	}
+
+	void ApplyStyleFromHolder(int index)
+	{
+		TileText.text = TileStyleHolder.Instance.TileStyles [index].Number.ToString();
+		TileText.color = TileStyleHolder.Instance.TileStyles [index].TextColor;
+		TileImage.color = TileStyleHolder.Instance.TileStyles [index].TileColor;
+	}
+
+	int CalcIndex(int num)
+	{
+		int count=0;
+		int calcNum = num;
+		for(;calcNum>=2;)
+		{
+			calcNum=calcNum/2;
+			count++;
+		}
+		if ((count - 1) < 0 || (count - 1) > 11) 
+		{
+			Debug.LogError ("Error Num=" + num);
+			return 0;
+		}
+		return count-1;
+	}
+
+	void ApplyStyle(int num)
+	{
+		ApplyStyleFromHolder (CalcIndex(num));
+	}
+
+	private void SetVisible()
+	{
+		TileImage.enabled = true;
+		TileText.enabled = true;
+	}
+
+	private void SetEmpty()
+	{
+		TileImage.enabled = false;
+		TileText.enabled = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
